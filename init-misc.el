39cb5c7bcb5d18a477bb50a119d2450904771909
Zach <zach_41@163.com>
2016-10-05 00:55:16 +0800

    fix some warnings

--- init-misc.el
+++ init-misc.el
@@ -1,11 +1,16 @@
-;;; -*- lexical-binding: t; -*-
+;;; init-misce.el --- miscellaneous functions -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
 
 ;; kill all buffers except exclu_buffers
-(defvar exclu_buffers '("*terminal<[0-9]\\{1,\\}>*" "*scratch*")
+(defvar zach-exclu_buffers '("*terminal<[0-9]\\{1,\\}>*" "*scratch*")
   "Exlusive buffers.")
 
 (defun kill-buffers-all ()
-  "Kill all active buffers except those names matched in exclu_buffers"
+  "Kill all active buffers except those names matched in exclu_buffers."
   (interactive)
   (setq list (buffer-list))
   (while list
@@ -13,7 +18,7 @@
 	   (name (buffer-name buffer)))
       (when (and name
 	   (not (string-equal name ""))
-	   (not (reduce #'(lambda (x y) (or x y)) (mapcar #'(lambda (str) (string-match str name)) exclu_buffers)))
+	   (not (reduce #'(lambda (x y) (or x y)) (mapcar #'(lambda (str) (string-match str name)) zach-exclu_buffers)))
 	   (kill-buffer buffer)
 	   )))
     (setq list (cdr list))))
@@ -23,13 +28,13 @@
   "Variable controlling read-only-open.")
 
 (defun toggle-read-source-p ()
-  "change the read-source flag"
+  "Change the read-source flag."
   (interactive)
   (if zach-read-source-code-p (setq zach-read-source-code-p nil) (setq zach-read-source-code-p t))
   (if zach-read-source-code-p (message "read source mode is on") (message "read source mode is off")))
 
 (defun read-only-open ()
-  (when read-source-code-p
+  (when zach-read-source-code-p
     (progn
       (message "File %s is now read-only" (buffer-file-name))
       (setq buffer-read-only t))
@@ -38,3 +43,5 @@
 (add-hook 'find-file-hook 'read-only-open)
 
 (provide 'init-misc)
+
+;;; init-misc.el ends here
